51f8fa2c9e03fa56bb797688c7b37379dffe4af6
bazylevnik0 <bazylevnik0@gmail.com>
2023-07-26 18:12:13 +0300

    add a build functional for field

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import { Text, View, StyleSheet, Button, Alert, TouchableOpacity} from 'react-native';
 import Constants from 'expo-constants';
 
 //https://github.com/iddan/react-native-canvas/ 
 import Canvas from 'react-native-canvas';
 
+const styles = StyleSheet.create({
+  containerView: {
+    flex: 12,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
+  },
+  headerView: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  fieldView: {
+    flex: 5,
+    width: '100%',
+    justifyContent: 'space-around',
+  },
+  fieldViewRow: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  weighView: {
+    flex: 5.25,
+    width: 300,
+    height: 250,
+  },
+  footerView: {
+    flex: 0.75,
+    width: '100%',
+    flexDirection: 'row',  
+    justifyContent: 'space-between',
+    backgroundColor: 'blue',
+  },
+  footerButton: {
+    width: '33.33%',
+  },
+  buttonVisible: {
+    color: 'blue',
+  },
+  buttonInvisible: {
+    backgroundColor: 'red',
+  },
+});
+
 export default function App() {
+  
   let field;
   let weigh;
   
@@ -16,25 +61,121 @@ export default function App() {
       canvas.width = 300;
       canvas.height = 250;
       weigh.draw("start");
-      console.log("asda")
+      console.log("asda");
     }
   }, [canvasRef]);
-  
+  const [buttons,setButtons] = useState([ 
+          <Button
+            title="00"
+            onPress={() => Alert.alert('Left button pressed')}
+          />,
+          <Button
+            title="01"
+            onPress={() => Alert.alert('Right button pressed')}
+          />,
+          <Button
+            title="02"
+            onPress={() => Alert.alert('Left button pressed')}
+          />,
+          <Button
+            title="03"
+            onPress={() => Alert.alert('Right button pressed')}
+          />,
+          <Button
+            title="04"
+            onPress={() => Alert.alert('Left button pressed')}
+          />,
+          <Button
+            title="05"
+            onPress={() => Alert.alert('Right button pressed')}
+          />,
+          <Button
+            title="06"
+            onPress={() => Alert.alert('Left button pressed')}
+          />,
+          <Button
+            title="07"
+            onPress={() => Alert.alert('Right button pressed')}
+          />,
+          <Button
+            title="08"
+            onPress={() => Alert.alert('Left button pressed')}
+          />,
+          <Button
+            title="09"
+            onPress={() => Alert.alert('Right button pressed')}
+          />,
+          <Button
+            title="10"
+            onPress={() => Alert.alert('Left button pressed')}
+          />,
+          <Button
+            title="11"
+            onPress={() => Alert.alert('Right button pressed')}
+          />,
+          <Button
+            title="12"
+            onPress={() => Alert.alert('Left button pressed')}
+          />,
+          <Button
+            title="13"
+            onPress={() => Alert.alert('Right button pressed')}
+          />,
+          <Button
+            title="14"
+            onPress={() => Alert.alert('Left button pressed')}
+          />,
+          <Button
+            title="15"
+            onPress={() => Alert.alert('Right button pressed')}
+          />,]);
+
+
   class Field {
-    rebuild(){
+    build(){
+      //calc new num
+      this.num = Math.floor(Math.random()*14)+2;
+    
+      for (let i = 0; i < this.num; i++) {
+         let title;
+         if(i<10){
+           title="0"+i;
+         } else title = ""+i;
+         buttons[i] = (<Button
+            title={title}
+            color="blue"
+            onPress={() => Alert.alert('Left button pressed')}
+            />);
+      }
+      for (let i = this.num; i < 16; i++) {
+         let title;
+         if(i<10){
+           title="0"+i;
+         } else title = ""+i;
+         buttons[i] = (<Button
+            title={title}
+            color="white"
+            disabled
+            onPress={() => Alert.alert('Left button pressed')}
+            />);
+      }
+      setButtons([...buttons]);// 
+
     }
     refresh(){
-        //without calculating new num
+      //without calculating new num
     }
     constructor() {
-        this.num  = Math.floor(Math.random()*16);
+        this.num = 0;
         this.selected = [];
+        this.elements = [];//!
     }
   }
 
   class Weigh {
     click(){
         console.log("click");
+        field.build();
     }
     draw(state){
         switch (state) {
@@ -60,7 +201,7 @@ export default function App() {
                 //text
                 ctx.fillStyle = 'black';
                 ctx.font = "50px serif";
-                ctx.fillText(this.count, 120, 245);
+                ctx.fillText(this.count, 121, 245);
                 break;
             case "current":
                 break;
@@ -94,76 +235,28 @@ export default function App() {
       </View>
       <View style={styles.fieldView}>
         <View style={styles.fieldViewRow}>
-          <Button
-            title="01"
-            onPress={() => Alert.alert('Left button pressed')}
-          />
-          <Button
-            title="02"
-            onPress={() => Alert.alert('Right button pressed')}
-          />
-          <Button
-            title="03"
-            onPress={() => Alert.alert('Left button pressed')}
-          />
-          <Button
-            title="04"
-            onPress={() => Alert.alert('Right button pressed')}
-          />
+          {buttons[0]}
+          {buttons[1]}
+          {buttons[2]}
+          {buttons[3]}
         </View>
         <View style={styles.fieldViewRow}>
-          <Button
-            title="05"
-            onPress={() => Alert.alert('Left button pressed')}
-          />
-          <Button
-            title="06"
-            onPress={() => Alert.alert('Right button pressed')}
-          />
-          <Button
-            title="07"
-            onPress={() => Alert.alert('Left button pressed')}
-          />
-          <Button
-            title="08"
-            onPress={() => Alert.alert('Right button pressed')}
-          />
+          {buttons[4]}
+          {buttons[5]}
+          {buttons[6]}
+          {buttons[7]}
         </View>
         <View style={styles.fieldViewRow}>
-          <Button
-            title="09"
-            onPress={() => Alert.alert('Left button pressed')}
-          />
-          <Button
-            title="10"
-            onPress={() => Alert.alert('Right button pressed')}
-          />
-          <Button
-            title="11"
-            onPress={() => Alert.alert('Left button pressed')}
-          />
-          <Button
-            title="12"
-            onPress={() => Alert.alert('Right button pressed')}
-          />
+          {buttons[8]}
+          {buttons[9]}
+          {buttons[10]}
+          {buttons[11]}
         </View>
         <View style={styles.fieldViewRow}>
-          <Button
-            title="13"
-            onPress={() => Alert.alert('Left button pressed')}
-          />
-          <Button
-            title="14"
-            onPress={() => Alert.alert('Right button pressed')}
-          />
-          <Button
-            title="15"
-            onPress={() => Alert.alert('Left button pressed')}
-          />
-          <Button
-            title="16"
-            onPress={() => Alert.alert('Right button pressed')}
-          />
+          {buttons[12]}
+          {buttons[13]}
+          {buttons[14]}
+          {buttons[15]}
         </View>
       </View>
       <View style={styles.weighView}>
@@ -201,41 +294,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  containerView: {
-    flex: 12,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingTop: Constants.statusBarHeight,
-  },
-  headerView: {
-    flex: 1,
-    alignSelf: 'stretch',
-  },
-  fieldView: {
-    flex: 5,
-    width: '100%',
-    justifyContent: 'space-around',
-  },
-  fieldViewRow: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  weighView: {
-    flex: 5.25,
-    width: 300,
-    height: 250,
-  },
-  footerView: {
-    flex: 0.75,
-    width: '100%',
-    flexDirection: 'row',  
-    justifyContent: 'space-between',
-    backgroundColor: 'blue',
-  },
-  footerButton: {
-    width: '33.33%',
-  }
-});
